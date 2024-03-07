d4172bc5554010e9fe7e8bc393491fed9f7efd70
João Victor <joao_nicolai@estudante.sesisenai.org.br>
2024-03-07 12:21:05 -0300

    Testes

--- two_class.js
+++ two_class.js
@@ -1,2 +1,16 @@
 class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+      }
+}
+
+class Moeda extends Obj{
+    dinheiro = 0
+}
+
+class Botao_1 extends Obj{
 }
